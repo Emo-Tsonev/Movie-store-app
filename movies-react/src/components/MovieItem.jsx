4b6c13d119e9f13dff3dd22ab091b80baec75fcd
DESKTOP-JMFDE6H\HP <emqta84@gmail.com>
2023-06-24 11:20:36 +0300

    create movie item

--- movies-react/src/components/MovieItem.jsx
+++ movies-react/src/components/MovieItem.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
-
+import styles from './MovieItem.module.css'
 
 const MovieItem = (props) => {
 
     const { movie } = props
 
-    return <div>
+    return <div className={styles.wrapper}>
         <Link to={`${movie.id}`}>{movie.title}</Link>
+        <div><img className={styles.poster} src={movie.posterUrl} alt="poster" /></div>
+        <div><p>{movie.description}</p></div>
+        
     </div>
 }
 
